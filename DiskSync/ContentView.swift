fb201aff2a21dc19a7509cdb65afa10ade64e09c
Yunus Emre <yunusemrekoyun26@gmail.com>
2026-06-29 17:32:01 +0300

    Initial Commit

---
+++ DiskSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiskSync
+//
+//  Created by Yunus Emre Koyun on 29.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiskSync/DiskSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskSyncApp.swift
+//  DiskSync
+//
+//  Created by Yunus Emre Koyun on 29.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct DiskSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
